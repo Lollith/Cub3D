99a4d3ca840e043754fde7a552f40a5ca6698730
Adeline GOUET <agouet@e3r10p23.clusters.42paris.fr>
2022-11-15 09:48:45 +0100

    clean pointeurs mlx et win, ESC segfault toujours

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: agouet <agouet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 11:50:48 by agouet            #+#    #+#             */
-/*   Updated: 2022/11/14 18:33:49 by agouet           ###   ########.fr       */
+/*   Updated: 2022/11/15 09:44:26 by agouet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,8 @@ int	main( void )
 	create_window(&data);
 	ft_key_loop_hook(&data);
 	mlx_loop(data.pt_mlx);
+	mlx_destroy_window(data.pt_mlx, data.pt_win);
+	mlx_destroy_display(data.pt_mlx);
+	free(data.pt_mlx);
 	return (0);
 }

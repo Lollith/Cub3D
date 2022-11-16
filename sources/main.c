116a92c833f4c7aad53bf9cdf4c8d243925beda8
agouet <adeline.gouet@gmail.com>
2022-11-16 09:58:34 +0100

    debut tuto

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: esmirnov <esmirnov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lollith <lollith@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 11:50:48 by agouet            #+#    #+#             */
-/*   Updated: 2022/11/15 16:55:32 by esmirnov         ###   ########.fr       */
+/*   Updated: 2022/11/16 09:58:18 by lollith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,29 @@ int	main(int ac, char **av)
 	// get map will be all.map.map = get_map(av) struct map to be deleted in main after that
 	// init_all(av, &all);
 	// all.window = data; //to be deleted because this stage has to be done in all init
+
+//tuto raycasting	
+	// double posX = 200;
+	// double posY = 200;
+	double dirX = -1;
+	double dirY = 0;
+	double planeX = 0;
+	double planeY = 0.66;
+	// double time = 0;
+	// double oldtime = 0;
+	int x = 0;
+	int lenght_plane =10; // w ds tuto valeur???
+	double ratioX; // cameraX
+	double raydirX;
+	double raydirY;
+	while (x < lenght_plane)
+	{
+		ratioX = 2 * x / (lenght_plane) - 1;
+		raydirX = dirX + planeX * ratioX;
+		raydirY = dirY + planeY * ratioX;
+		x++;
+	}
+
 	create_window(&data);
 	ft_key_loop_hook(&data);
 	mlx_loop(data.pt_mlx);

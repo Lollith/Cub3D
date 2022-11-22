16b185fe2619afb5c8dff6b254e57cbcc10c34e6
lollith <adeline.gouet@gmail.com>
2022-11-22 18:10:41 +0000

    correction

--- sources/render/move.c
+++ sources/render/move.c
@@ -6,7 +6,7 @@
 /*   By: lollith <lollith@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:23:02 by agouet            #+#    #+#             */
-/*   Updated: 2022/11/22 17:23:02 by lollith          ###   ########.fr       */
+/*   Updated: 2022/11/22 18:10:16 by lollith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // si N , touche up  : vers le haut
 //si E , fleche droite : vers la droite
 
-// move = change ma carte
+// move = ne change pas  ma carte, met a jour les pixels
 void	move(double new_position, t_all *all)
 {
 	all->pos.old_p_y = all->pos.p_y + 0.1;//+0,1 sinon se decale avec la new pos definie ds keypress

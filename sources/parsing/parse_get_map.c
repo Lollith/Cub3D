6b185297cbad348ffc9f2a66bdb3c25fc17ac76f
Elena SMIRNOVA <esmirnov@e1r2p19.clusters.42paris.fr>
2022-11-28 17:54:00 +0100

    temp  save

--- sources/parsing/parse_get_map.c
+++ sources/parsing/parse_get_map.c
@@ -6,16 +6,25 @@
 /*   By: esmirnov <esmirnov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 16:24:02 by esmirnov          #+#    #+#             */
-/*   Updated: 2022/11/28 16:02:23 by esmirnov         ###   ########.fr       */
+/*   Updated: 2022/11/28 17:51:26 by esmirnov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+int	ft_parse_map(t_map *map)
+{
+	char	**tab_tmp;
+	int		len;
+
+	
+	
+	return (0);
+}
 
 int		ft_get_map(char *line, t_map *map)
 {
-	int		len;
+	// int		len;
 	char	*tmp;
 
 	if (map->line == NULL)
@@ -31,10 +40,10 @@ int		ft_get_map(char *line, t_map *map)
 		print_error_fd("ft_get_map: strdup or strjoin failed", NULL, 2);
 		return (1);
 	}
-	len = ft_strlen(line);
-	if (map->x < len)
-		map->x = len;
-	map->y++;
-	return (0);
+	// len = ft_strlen(line);
+	// if (map->x < len)
+	// 	map->x = len;
+	// map->y++;
+	return (ft_parse_map(map));
 }
 // *ft_strjoin_no_free(char const *s1, char const *s2)
